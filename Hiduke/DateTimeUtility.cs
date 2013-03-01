fcf28c6feddd33b08f6fa7d30935b6b5f6164e9f
michi1129 <ro.michi1129@gmail.com>
2013-03-01 18:09:11 +0900

    add first application

---
+++ Hiduke/DateTimeUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hiduke
+{
+    public static class DateTimeUtility
+    {
+        public static long DateTimeToUnixTime(DateTime dt)
+        {
+            DateTime d1 = new DateTime(1970, 1, 1);
+            DateTime d2 = dt.ToUniversalTime();
+            TimeSpan ts = new TimeSpan(d2.Ticks - d1.Ticks);
+            return (long)Math.Round(ts.TotalMilliseconds, 0);
+        }
+
+        public static DateTime JsonFormatToDateTime(string s)
+        {
+            string pattern = @"\/Date\((-?\d+)\)\/";
+            System.Text.RegularExpressions.Match match = System.Text.RegularExpressions.Regex.Match(s, pattern);
+            return match.Groups[1].ToString().UnixTimeToDateTime();
+        }
+
+        public static DateTime UnixTimeToDateTime(this string s)
+        {
+            long l = long.Parse(s);
+            return new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(l).ToLocalTime();
+        }
+    }
+}
---
+++ Hiduke/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Globalization;
+
+namespace Hiduke
+{
+    public partial class Form1 : Form
+    {
+        private string format_ = "yyyy/MM/dd HH:mm:ss";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime dt = new DateTime(1900, 1, 1);
+            long l = DateTimeUtility.DateTimeToUnixTime(dt);
+            string s = string.Format("/Date({0})/", l);
+
+            textBox1.Text = dt.ToString(format_);
+            textBox2.Text = s;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            DateTime outDate;
+            bool dateParseResult = DateTime.TryParseExact(value, format_, null, DateTimeStyles.None, out outDate);
+            long l = DateTimeUtility.DateTimeToUnixTime(outDate);
+            string s = string.Format("/Date({0})/", l);
+            textBox2.Text = s;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string value = textBox2.Text;
+            DateTime dt = DateTimeUtility.JsonFormatToDateTime(value);
+            textBox1.Text = dt.ToString(format_);
+        }
+    }
+}
